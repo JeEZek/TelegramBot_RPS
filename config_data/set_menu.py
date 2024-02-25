d17898eaff8415e9df3b57df9da71524428aaeb8
Kirill Pomaskin <kirill.pomaskin@gmail.com>
2024-02-25 12:19:29 +0200

    Add menu

---
+++ config_data/set_menu.py
@@ -0,0 +1,19 @@
+import logging
+
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+from lexicon.lexicon_ru import LEXICON_COMMANDS_RU
+
+logger = logging.getLogger(__name__)
+
+async def set_main_menu(bot: Bot):
+
+    main_menu_commands = [
+        BotCommand(
+            command=command,
+            description=description
+        ) for command, description in LEXICON_COMMANDS_RU.items()
+    ]
+    logger.info(f'Creating menu commands {main_menu_commands}')
+    await bot.set_my_commands(main_menu_commands)
